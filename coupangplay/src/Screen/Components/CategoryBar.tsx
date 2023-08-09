1364fde750f3137a99ae43074f97834115b51cb3
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-09 22:11:46 +0900

    added categories bar animation

--- coupangplay/src/Screen/Components/CategoryBar.tsx
+++ coupangplay/src/Screen/Components/CategoryBar.tsx
@@ -10,27 +10,17 @@ import {
 import { CATEGORIES } from "../Types/CategoryBarTypes";
 import { AnimatePresence } from "framer-motion";
 
-//0 - 10 / 10 - 23
-
 function CategoryBar() {
-    const [startIndex, setStartIndex] = useState<number>(0);
-    const [endIndex, setEndIndex] = useState<number>(10);
+    const [categoryIndex, setCategoryIndex] = useState<number>(0);
     const [direction, setDirection] = useState<number>(1);
     const [moving, setMoving] = useState<boolean>(false);
+    const categories = [CATEGORIES.slice(0, 10), CATEGORIES.slice(10, 23)];
 
     function updateIndex() {
-        if (startIndex === 0) {
-            setStartIndex(10);
-            setEndIndex(23);
-        } else {
-            setStartIndex(0);
-            setEndIndex(10);
-        }
+        setCategoryIndex((prev) => (prev ? 0 : 1));
     }
 
     function onLeftArrowClick() {
-        // if (moving) return;
-
         updateIndex();
         setDirection(-1);
         setMoving(true);
@@ -38,53 +28,47 @@ function CategoryBar() {
     }
 
     function onRightArrowClick() {
-        // if (moving) return;
-
         updateIndex();
         setDirection(1);
-        setMoving(true);
 
         console.log("right arrow clicked");
     }
 
-    function animationFinished() {
-        setMoving(false);
-        console.log("animation finished");
-    }
-
     return (
-        <AnimatePresence
-            // initial={false}
-            onExitComplete={animationFinished}
-            // custom={direction}
-        >
-            <Container>
-                <LeftArrow
-                    onClick={onLeftArrowClick}
-                    src={LEFT_ARROW_URL}
-                ></LeftArrow>
-                {CATEGORIES.slice(startIndex, endIndex).map((category) => (
-                    <Category
-                        length={category.length * 15}
-                        // variants={GenresCategoriesVariants}
-                        // initial="start"
-                        // animate="end"
-                        // exit="exit"
-                        // custom={direction}
-                        // transition={{
-                        //     type: "tween",
-                        //     duration: 1.5,
-                        // }}
-                    >
-                        {category}
-                    </Category>
-                ))}
-                <RightArrow
-                    onClick={onRightArrowClick}
-                    src={RIGHT_ARROW_URL}
-                ></RightArrow>
-            </Container>
-        </AnimatePresence>
+        <>
+            <AnimatePresence initial={false} custom={direction} mode="wait">
+                <Container>
+                    <LeftArrow
+                        onClick={onLeftArrowClick}
+                        src={LEFT_ARROW_URL}
+                    ></LeftArrow>
+
+                    {categories[categoryIndex].map((category) => (
+                        <Category
+                            key={category}
+                            layoutId={category}
+                            length={category.length * 15}
+                            variants={GenresCategoriesVariants}
+                            initial="start"
+                            animate="end"
+                            exit="exit"
+                            custom={direction}
+                            transition={{
+                                type: "tween",
+                                duration: 1,
+                            }}
+                        >
+                            {category}
+                        </Category>
+                    ))}
+
+                    <RightArrow
+                        onClick={onRightArrowClick}
+                        src={RIGHT_ARROW_URL}
+                    ></RightArrow>
+                </Container>
+            </AnimatePresence>
+        </>
     );
 }
 
