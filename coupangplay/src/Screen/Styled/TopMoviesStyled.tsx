81c8eae1c0c0d38b351159c37c27ca88d5a5c6fb
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-11 13:33:41 +0900

    fixed animation error at top20 movies

--- coupangplay/src/Screen/Styled/TopMoviesStyled.tsx
+++ coupangplay/src/Screen/Styled/TopMoviesStyled.tsx
@@ -1,12 +1,12 @@
-import { motion } from "framer-motion";
+import { motion, motionValue } from "framer-motion";
 import { styled } from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
     height: 210px;
-    padding: 0px 30px;
     border: 1px solid azure;
     box-sizing: border-box;
+    overflow: hidden;
     position: relative;
 `;
 
@@ -14,6 +14,7 @@ export const Header = styled.div`
     width: 100%;
     height: 20px;
     font-size: 18px;
+    padding-left: 40px;
     border: 1px solid azure;
 `;
 
@@ -51,29 +52,34 @@ export const RightArrow = styled.img`
     }
 `;
 
-export const Frames = styled.div`
-    width: 100%;
+export const Frames = styled(motion.div)`
+    width: 95%;
     height: 180px;
     display: grid;
     grid-template-columns: repeat(7, 1fr);
+    gap: 15px;
+    margin-top: 10px;
     box-sizing: border-box;
     overflow: hidden;
-    gap: 15px;
-    padding: 0 10px;
-    margin-top: 10px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    position: absolute;
+
     border: 1px solid blueviolet;
 `;
 
-export const Frame = styled(motion.div)`
+export const Frame = styled.div`
     height: 180px;
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-start;
     align-items: end;
     box-sizing: border-box;
+    overflow-x: hidden;
     background-color: rgba(255, 192, 203, 0.1);
 `;
 
-export const Rank = styled(motion.div)`
+export const Rank = styled.div`
     width: 50%;
     height: 100%;
     display: flex;
@@ -82,16 +88,41 @@ export const Rank = styled(motion.div)`
     color: black;
     font-size: 50px;
     font-weight: bold;
-
     text-shadow: -1px 1px 8px ${(props) => props.theme.coupangBlue};
     /* background-color: pink; */
 `;
 
-export const Poster = styled(motion.div)<{ BGPhoto?: string }>`
+export const Poster = styled.div<{ BGPhoto?: string }>`
     width: 70%;
     height: 110%;
     background-image: url(${(props) => props.BGPhoto});
     background-size: cover;
     background-position: center;
     border-radius: 5px;
+`;
+
+export const RowVariants = {
+    start: {
+        x: window.outerWidth + 5,
+    },
+    end: {
+        x: 0,
+    },
+    exit: {
+        x: -window.outerWidth - 5,
+    },
+};
+
+export const TestRow = styled(motion.div)`
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 10px;
+    position: absolute;
+`;
+
+export const TestBox = styled(motion.div)`
+    height: 200px;
+    background-color: whitesmoke;
+    border-radius: 10px;
 `;
