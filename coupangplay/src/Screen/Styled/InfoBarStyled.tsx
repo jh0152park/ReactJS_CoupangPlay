430e5c49844c3bfe5c30048a7058ba526b80ec47
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-13 00:55:30 +0900

    added title and socre at info bar

--- coupangplay/src/Screen/Styled/InfoBarStyled.tsx
+++ coupangplay/src/Screen/Styled/InfoBarStyled.tsx
@@ -9,9 +9,12 @@ export const Container = styled(motion.div)`
     overflow: hidden;
     position: absolute;
     bottom: 0;
-    opacity: 0;
+    opacity: 1;
     border-bottom-left-radius: 2px;
     border-bottom-right-radius: 2px;
+    /* display: flex;
+    align-items: center;
+    justify-content: flex-start; */
 `;
 
 export const InfoBarVariant = {
@@ -24,3 +27,30 @@ export const InfoBarVariant = {
         },
     },
 };
+
+export const Title = styled.div`
+    box-sizing: border-box;
+    overflow: hidden;
+    width: 100%;
+    height: 50%;
+    font-size: 18px;
+    text-align: left;
+    padding: 5px;
+    background-color: pink;
+`;
+
+export const Description = styled.div`
+    width: 100%;
+    height: 50%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 15px;
+    padding-left: 5px;
+    background-color: darkorange;
+`;
+
+export const Star = styled.span`
+    color: ${(props) => props.theme.coupangBlue};
+    font-size: 10px;
+`;
