ce9e34ad718e7d6ac649d1f0fa068efa150c1c6e
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-13 02:40:28 +0900

    added switch screen file to render new screen

---
+++ coupangplay/src/SwitchScreen.tsx
@@ -0,0 +1,27 @@
+import { Route, Switch, useLocation } from "react-router-dom";
+import Header from "./Screen/Components/Header";
+import Home from "./Screen/Home";
+import MovieDetail from "./Screen/MovieDetail";
+
+function SwitchScreen() {
+    const Location = useLocation();
+    const DetailMatch = Location.pathname.startsWith("/movies/details");
+    const MovieId = DetailMatch ? Location.pathname.split("/")[3] : "n/a";
+
+    return (
+        <>
+            <Header />
+            <Switch>
+                <Route path={`/movies/details/${MovieId}`}>
+                    <MovieDetail></MovieDetail>
+                </Route>
+
+                <Route path={["/", "/영화"]}>
+                    <Home></Home>
+                </Route>
+            </Switch>
+        </>
+    );
+}
+
+export default SwitchScreen;
