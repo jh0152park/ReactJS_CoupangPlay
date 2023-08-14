1b131e0fa085b4555b8b4bdacf95befd6b2ecab8
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-14 19:54:06 +0900

    added search interface type

--- coupangplay/src/GlobalFeatures.tsx
+++ coupangplay/src/GlobalFeatures.tsx
@@ -142,3 +142,37 @@ export interface ITVResult {
 export interface ITVData {
     results: ITVResult[];
 }
+
+export interface IMovieSearchResult {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+export interface ITVSearchResult {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    origin_country: string[];
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    first_air_date: string;
+    name: string;
+    vote_average: number;
+    vote_count: number;
+}
